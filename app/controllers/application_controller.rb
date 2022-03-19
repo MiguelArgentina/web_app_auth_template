dc31f567af6b6020d9e8ad4ef9d7262d5d77c4f2
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2022-03-18 23:14:06 -0300

    Crate template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   include Authentication
+  before_action :set_current_user
+
+  def set_current_user
+    @current_user = current_user
+  end
 end
